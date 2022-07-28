ae131cdc8a788fb904a6f3e2e504168cbe0341be
Leo Peron <leo.peron@etu.utc.fr>
2022-07-28 16:48:45 +0200

    updated detail and starting post/patch

--- api/ddrs_api/urls.py
+++ api/ddrs_api/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('questionnaires/', views.questionnaires_list),
     path('questionnaires/<int:pk>/', views.questionnaire_detail),
-    path('questions/', views.questions_list),
-    path('questions/<int:pk>/', views.question_detail)
+    path('questions/', views.QuestionList.as_view()),
+    path('questions/<str:type>/', views.QuestionListByType.as_view()),
+    path('questions/<str:type>/<int:pk>/', views.QuestionDetail.as_view())
 ]
--- api/ddrs_api/views.py
+++ api/ddrs_api/views.py
@@ -1,6 +1,9 @@
 from curses.ascii import HT
 from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
+from rest_framework.views import APIView
 from ddrs_api.models import Questionnaire
 from ddrs_api.models import Question, QuestionChoixMultiple, QuestionSlider, QuestionLibre
 from ddrs_api.serializers import QuestionnaireSerializer
@@ -19,6 +22,7 @@ def questionnaires_list(request):
     # Error if not GET
     return HttpResponse(status = 400)
 
+
 @csrf_exempt
 def questionnaire_detail(request, pk):
     """
@@ -36,45 +40,81 @@ def questionnaire_detail(request, pk):
     # Error if not GET
     return HttpResponse(status = 400)
 
-@csrf_exempt
-def questions_list(request):
+class QuestionList(APIView):
     """
     List all Questions
     """
-    if request.method == 'GET':
 
+    types = [ QuestionSlider, QuestionLibre, QuestionChoixMultiple]
+    serializers = [ QuestionSliderSerializer, QuestionLibreSerializer, QuestionChoixMultipleSerializer]
+    names = [ 'slider', 'libre', 'qcm']
+
+    def get(self, request, format=None):
         datas = {}
-        types = [ QuestionSlider, QuestionLibre, QuestionChoixMultiple]
-        serializers = [ QuestionSliderSerializer, QuestionLibreSerializer, QuestionChoixMultipleSerializer]
-        names = [ 'sliders', 'libre', 'qcms']
-
-        for type, serial,name in zip(types, serializers, names):
+        for type, serial,name in zip(QuestionList.types, QuestionList.serializers, QuestionList.names):
             questions = type.objects.all()
             datas[name] = serial(questions, many=True).data
-
         # Merging
         return JsonResponse(datas, safe=False)
 
-    # Error if not GET
-    return HttpResponse(status = 400)
-
-@csrf_exempt
-def question_detail(request, pk):
+class QuestionListByType(APIView):
     """
-    Retrieve data about a specific Question
+    List all Questions or create new question by type
+    """
+
+    types = [ QuestionSlider, QuestionLibre, QuestionChoixMultiple]
+    serializers = [ QuestionSliderSerializer, QuestionLibreSerializer, QuestionChoixMultipleSerializer]
+    names = [ 'slider', 'libre', 'qcm']
+
+    def get_tools(self, type):
+        if type not in QuestionListByType.names:
+            return HttpResponse(status = 404)
+        i = QuestionListByType.names.index(type)
+        return QuestionListByType.types[i], QuestionListByType.serializers[i]
+
+    def get(self, request, type, format=None):
+        model, serializer = self.get_tools(type)
+        questions = model.objects.all()
+        serialize = serializer(questions, many=True)
+        return JsonResponse(serialize.data, safe=False)
+
+    def post(self, request, type, format=None):
+        entry_data = request.data
+        serializer = self.get_tools(type)[1]
+        serialize = serializer(data=entry_data)
+        if serialize.is_valid():
+            serialize.save()
+            return JsonResponse(serialize.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serialize.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class QuestionDetail(APIView):
+    """
+    Retrieve, update or delete a Question
     """
     types = [ QuestionSlider, QuestionLibre, QuestionChoixMultiple]
     serializers = [ QuestionSliderSerializer, QuestionLibreSerializer, QuestionChoixMultipleSerializer]
+    names = [ 'slider', 'libre', 'qcm']
 
-    if request.method == 'GET':
-        for type, serial in zip(types, serializers):
-            try:
-                question = type.objects.get(pk = pk)
-                serializer = serial(question)
-                return JsonResponse(serializer.data, safe=False)
-            except type.DoesNotExist:
-                continue
-        return HttpResponse(status = 404)
+    def get_object(self, type, pk):
+        i = QuestionDetail.names.index(type)
+        try:
+            return QuestionDetail.types[i].objects.get(pk = pk), QuestionDetail.serializers[i]
+        except Snippet.DoesNotExist:
+            raise HttpResponse(status = 404)
 
-    # Error if not GET
-    return HttpResponse(status = 400)
+    def is_editable(self, question):
+        questionnaire = Questionnaire.objects.get(pk = question.questionnaire_id)
+        # Check if the related Questionnaire is on
+        return not questionnaire.MONTHSTART_START <= datetime.now() <= questionnaire.MONTHSTART_END and not questionnaire.MONTHEND_START <= datetime.now() <= questionnaire.MONTHEND_END
+
+    def get(self, request, type, pk, format=None):
+        question, serial = self.get_object(type, pk)
+        serializer = serial(question)
+        return JsonResponse(serializer.data, safe=False)
+
+    def delete(self, request, pk, format=None):
+        question = self.get_object(pk)
+        if self.is_editable(question):
+            snippet.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
