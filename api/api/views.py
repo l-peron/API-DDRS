b8ba5cb14705b8d9332974d98271994fbd81b309
yuyujijin <eugenevalty@gmail.com>
2022-07-31 18:48:08 +0200

    suppression de routes inutiles

--- api/api/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from ddrs_api.models import User
-
-def index(request):
-    if request.user.is_authenticated:
-        return HttpResponse('<p><p>You logged in as <strong>%s</strong>.</p><p><a href="/accounts/logout">Logout</a></p>' % request.user)
-    else:
-        return HttpResponse('<p><a href="/accounts/login">Login</a></p>')
