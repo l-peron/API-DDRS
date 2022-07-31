634bc493861df59eca16a5ef2d52b033cb2d30c2
yuyujijin <eugenevalty@gmail.com>
2022-07-31 19:37:29 +0200

    ajout des endpoints pour créer (/questionnaires/ en POST) et supprimer (/questionnaires/{id} en DELETE)

--- api/ddrs_api/urls.py
+++ api/ddrs_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('questionnaires/', views.questionnaires_list),
-    path('questionnaires/<int:pk>/', views.questionnaire_detail)
+    path('questionnaires/', views.questionnaires_list.as_view()),
+    path('questionnaires/<int:pk>/', views.questionnaire_detail.as_view()),
 ]
--- api/ddrs_api/views.py
+++ api/ddrs_api/views.py
@@ -1,35 +1,73 @@
 from curses.ascii import HT
+import json
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from ddrs_api.models import Questionnaire
 from ddrs_api.serializers import QuestionnaireSerializer
+from rest_framework.views import APIView
 
-@csrf_exempt
-def questionnaires_list(request):
-    """
-    List all Questionnaires
-    """
-    if request.method == 'GET':
+class questionnaires_list(APIView):
+    def get(self, request):
+        """
+        List all Questionnaires
+        """
         questionnaires = Questionnaire.objects.all()
         serializer = QuestionnaireSerializer(questionnaires, many=True)
         return JsonResponse(serializer.data, safe=False)
-    
-    # Error if not GET
-    return HttpResponse(status = 400)
 
-@csrf_exempt
-def questionnaire_detail(request, pk):
-    """
-    Retrieve data about a specific Questionnaire
-    """
-    try:
-        questionnaire = Questionnaire.objects.get(pk = pk)
-    except Questionnaire.DoesNotExist:
-        return HttpResponse(status = 404)
-    
-    if request.method == 'GET':
+    def post(self, request):
+        """
+        Creates a Questionnaire with received fields and return its informations
+        
+        Infos are received following this format :
+        {
+            'title_text' : String,
+            'MONTHSTART_START' : Date,
+            'MONTHSTART_END' : Date,
+            'MONTHEND_START' : Date,
+            'MONTHEND_END' : Date
+        }
+        """
+        post_data = json.loads(request.body)
+
+        try:
+            questionnaire = Questionnaire(
+                title_text = post_data['title_text'], 
+                MONTHSTART_START = post_data['MONTHSTART_START'],
+                MONTHSTART_END = post_data['MONTHSTART_END'],
+                MONTHEND_START = post_data['MONTHEND_START'],
+                MONTHEND_END = post_data['MONTHEND_END']
+            )
+
+            questionnaire.save()
+            serializer = QuestionnaireSerializer(questionnaire)
+            
+            return JsonResponse(serializer.data)
+        except KeyError:
+            # 400 Bad Request
+            return HttpResponse(status = 400)
+
+class questionnaire_detail(APIView):
+    def get(self, request, *args, **kwargs):
+        """
+        Retrieve data about a specific Questionnaire
+        """
+        try:
+            questionnaire = Questionnaire.objects.get(pk = kwargs['pk'])
+        except Questionnaire.DoesNotExist:
+            return HttpResponse(status = 404)
+        
         serializer = QuestionnaireSerializer(questionnaire)
         return JsonResponse(serializer.data)
 
-    # Error if not GET
-    return HttpResponse(status = 400)
+    def delete(self, request, *args, **kwargs):
+        """
+        Delete a specific Questionnaire
+        """
+        try:
+            questionnaire = Questionnaire.objects.get(pk = kwargs['pk'])
+        except Questionnaire.DoesNotExist:
+            return HttpResponse(status = 404)
+
+        questionnaire.delete()
+        return HttpResponse(status = 200)
