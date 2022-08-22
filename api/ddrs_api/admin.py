25118bb056e10a6aa60c8b33bf193ca2c285d268
jumelloi <loic.jumel@etu.utc.fr>
2022-08-22 17:19:17 +0200

    Add app models to admin

--- api/ddrs_api/admin.py
+++ api/ddrs_api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Questionnaire)
+admin.site.register(Question)
+admin.site.register(QuestionSlider)
+admin.site.register(QuestionChoixMultiple)
+admin.site.register(QCMChamp)
+admin.site.register(QuestionLibre)
+admin.site.register(Reponse)
+admin.site.register(ReponseSlider)
+admin.site.register(ReponseChoixMultiple)
+admin.site.register(RCMChamp)
+admin.site.register(ReponseLibre)
