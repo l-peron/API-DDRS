49c94dfabca996fe1019e95ac699b03cf15c3c23
jumelloi <loic.jumel@etu.utc.fr>
2022-08-22 16:28:35 +0200

    Fix admin url

--- api/ddrs_api/urls.py
+++ api/ddrs_api/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     path('users/', views.user_list),
     path('users/<int:pk>/', views.user_detail),
-    path('admin/', admin.site.urls),
     path('accounts/login', django_cas_ng.views.LoginView.as_view(), name='cas_ng_login'),
     path('accounts/logout', django_cas_ng.views.LogoutView.as_view(), name='cas_ng_logout'),
     path('questionnaires/', views.questionnaires_list.as_view()),
