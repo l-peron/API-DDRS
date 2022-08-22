076af019050e2d40749b6aa29c98317a1a3582ad
jumelloi <loic.jumel@etu.utc.fr>
2022-08-22 17:42:48 +0200

    Auto add app models to admin

--- api/ddrs_api/admin.py
+++ api/ddrs_api/admin.py
@@ -1,14 +1,2 @@
 from django.apps import apps, AppConfig
 from django.contrib import admin
-
-
-class CustomApp(AppConfig):
-    name = 'ddrs_api'
-
-    def ready(self):
-        models = apps.get_models()
-        for model in models:
-            try:
-                admin.site.register(model)
-            except admin.sites.AlreadyRegistered:
-                pass
--- api/ddrs_api/apps.py
+++ api/ddrs_api/apps.py
@@ -1,6 +1,15 @@
-from django.apps import AppConfig
+from django.apps import apps, AppConfig
+from django.contrib import admin
 
 
 class DdrsApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ddrs_api'
+
+    def ready(self):
+        models = self.get_models()
+        for model in models:
+            try:
+                admin.site.register(model)
+            except admin.sites.AlreadyRegistered:
+                pass
